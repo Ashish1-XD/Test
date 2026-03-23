6fae1ce72983d1a4340f84cca7472a40bd6cadf7
Ashish <ashishj.inmca2227@saintgits.org>
2026-03-23 11:29:58 +0530

    Add Hello World program in testpull.c

---
+++ testpull.c
@@ -0,0 +1,6 @@
+#include <stdio.h> // Preprocessor command to include standard I/O library
+
+int main() { // Main function where program execution begins
+    printf("Hello, World!\n"); // Displays output
+    return 0; // Indicates that the program ended successfully
+}
